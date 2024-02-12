89555b1810699916e63c06ec8e6dd3b1d1b0b4d3
zaehorang <jaho001@uos.ac.kr>
2024-02-12 18:40:14 +0900

    chore: 프로젝트 개별 생성

---
+++ Algoritm by CPP/Algoritm by CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algoritm by CPP
+//
+//  Created by zaehorang on 2024/02/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
